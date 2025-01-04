2472b5e2a4630211198b0e767a3d3c34d6da27e8
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-01-04 18:55:43 +0100

    Add <Link> to logo component

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,8 +1,16 @@
-export default async function Page({
-	params,
-}: {
-	params: Promise<{ city: string }>
-}) {
+import H1 from '@/components/h1'
+
+type PageProps = {
+	params: {
+		city: string
+	}
+}
+export default async function Page({ params }: PageProps) {
 	const city = (await params).city
-	return <main>Hello events page, city is: {city}</main>
+
+	return (
+		<main>
+			<H1>Events in {city}</H1>
+		</main>
+	)
 }
---
+++ src/components/h1.tsx
@@ -0,0 +1,7 @@
+export default function H1({ children }: { children: React.ReactNode }) {
+	return (
+		<h1 className="text-3xl font-bold tracking-tight lg:text-6xl">
+			{children}
+		</h1>
+	)
+}
