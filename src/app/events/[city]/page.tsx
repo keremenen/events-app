49c5c28e13b1dfb5aff2b95438c17ec0d83d5e5d
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-01-04 20:07:10 +0100

    Change h1 in event page

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -9,8 +9,12 @@ export default async function Page({ params }: PageProps) {
 	const city = (await params).city
 
 	return (
-		<main>
-			<H1>Events in {city}</H1>
+		<main className="flex flex-col items-center py-24 px-5">
+			<H1>
+				{city === 'all' && 'All events'}
+				{city !== 'all' &&
+					'Events in ' + city.charAt(0).toUpperCase() + city.slice(1)}
+			</H1>
 		</main>
 	)
 }
