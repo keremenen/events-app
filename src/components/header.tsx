d4ff5b3dadcf250ee09856429b7e9761613ba07d
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-01-04 18:36:05 +0100

    Add active link styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import Link from 'next/link'
 import Logo from './logo'
+import { usePathname } from 'next/navigation'
+import clsx from 'clsx'
+import { motion } from 'framer-motion'
 
 const routes = [
 	{
@@ -13,18 +18,29 @@ const routes = [
 ]
 
 export default function Header() {
+	const currentPathname = usePathname()
+
 	return (
 		<header className="flex justify-between items-center border-b border-white/10 h-14 px-3 md:px-9">
 			<Logo />
 
-			<nav>
-				<ul className="flex gap-x-6 text-sm">
+			<nav className="h-full">
+				<ul className="flex gap-x-6 text-sm h-full items-center">
 					{routes.map((route) => (
 						<li
 							key={route.path}
-							className="text-white/50 hover:text-white  transition"
+							className={clsx(' hover:text-white transition relative', {
+								'text-white': currentPathname === route.path,
+								'text-white/50': currentPathname !== route.path,
+							})}
 						>
 							<Link href={route.path}>{route.name}</Link>
+							{currentPathname === route.path && (
+								<motion.div
+									className="bg-accent h-1 absolute -bottom-2 w-full"
+									layoutId="header-active-link"
+								/>
+							)}
 						</li>
 					))}
 				</ul>
